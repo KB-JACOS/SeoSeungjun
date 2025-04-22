503007087ca1665df9e00360b0302d507a64ee19
greensnapback0229 <smdmim@gmail.com>
2025-04-22 16:53:31 +0900

    solved: baekjoon_12919 - A와 B 2

---
+++ baekjoon_12919.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Main {
+    static Queue<char[]> queue = new LinkedList<>();
+    
+    static String T, S;
+    static int result = 0;
+    public static void main(String args[]) throws Exception{
+        
+        Scanner sc = new Scanner(System.in);
+
+        S = sc.nextLine();
+        T = sc.nextLine();
+
+        recur(T);
+
+        System.out.println(result);
+        sc.close();
+    }
+
+    public static void recur(String str) {
+        
+        if(str.equals(S)){
+            // System.out.println(str);
+            result = 1;
+            return;
+        } 
+        
+        if(str.length() == S.length()){
+            return;
+        }
+
+        if(str.endsWith("A")){
+            recur(str.substring(0, str.length()-1));
+        }
+
+        if (str.startsWith("B")) {
+            recur(new StringBuilder(str.substring(1)).reverse().toString());
+        }        
+
+    }
+}
