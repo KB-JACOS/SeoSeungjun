1540dcabc6435e2494c56cd0c724cbf8271131e3
greensnapback0229 <smdmim@gmail.com>
2025-05-14 09:50:02 +0900

    solved: swea_24001 - 로봇 언어

---
+++ swea_24001.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.util.*;
+class swea_24001 {
+    
+    static int distance = 0, max = 0;
+   public static void main(String args[]) throws Exception {
+      Scanner sc = new Scanner(System.in);
+      int T;
+      T=sc.nextInt();
+        
+        sc.nextLine();
+        
+      for(int test_case = 1; test_case <= T; test_case++) {
+         String command = sc.nextLine();
+            char[] cmd = command.toCharArray();
+            searchOnlyCommand(cmd);
+            System.out.println(max);
+            max = 0;
+      }
+   }
+    
+    public static void searchOnlyCommand(char[] cmd) {
+        
+        int L = 0, R = 0;
+        int questionMarkCnt = 0;
+        for(int i=0;i<cmd.length;i++){
+            if(cmd[i] == 'R'){
+                R++;
+            }
+            else if (cmd[i] == 'L'){
+                L++;
+            }
+            
+            else {
+                questionMarkCnt++;
+                
+            }
+            //System.out.print( " " + distance);
+            max = Math.max(Math.abs(R-L) + questionMarkCnt, max);
+        }
+    }
+ }
