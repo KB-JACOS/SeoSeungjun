175b83d543d975f572161ba822b9eee50fbefdbf
greensnapback0229 <smdmim@gmail.com>
2025-05-15 17:44:40 +0900

    solved: baekjoon_2805 - 나무 자르기

---
+++ baekjoon_2805.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main{
+    public static void main(String[] args) throws Exception { 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int M, N; 
+
+        StringTokenizer st = new StringTokenizer(br.readLine()); 
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        
+        st = new StringTokenizer(br.readLine());
+
+        int[] tree = new int[N];
+
+        int left = 0, right = Integer.MIN_VALUE, mid = 0;
+        for(int i=0;i<N;i++) {
+            tree[i] = Integer.parseInt(st.nextToken());
+            right = Math.max(right, tree[i]);
+        }
+
+        int result = 0;
+        while(left <= right) {
+            mid = (left + right) / 2;
+
+            long sum = 0;
+            for(int t : tree) {
+                if(mid < t) sum += t - mid;
+            }
+
+            if(sum >= M){
+                result = mid;
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
