c89e2f650f624f74c1b72fa90d1db3555dde7658
greensnapback0229 <smdmim@gmail.com>
2025-05-26 23:01:43 +0900

    solved: leetcode_322 - coin changed

---
+++ leetcode_322.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class leetcode_322 {
+    public int coinChange(int[] coins, int amount) {
+        Deque<int[]> queue = new ArrayDeque<>(); 
+
+        queue.offer(new int[] {amount, 0});
+
+        boolean[] visited = new boolean[amount+1];
+        while(!queue.isEmpty()){
+            int[] cur = queue.poll();
+            int left = cur[0];
+            int cnt = cur[1];
+
+            if(left == 0) return cnt;
+
+            for(int i=0;i<coins.length;i++){
+                int next = left - coins[i];
+                if(next < 0 || visited[next]){ continue; }
+                visited[next] = true;
+                queue.offer(new int[] {next, cnt+1});
+            }
+        }
+        return -1;   
+    }
+}
