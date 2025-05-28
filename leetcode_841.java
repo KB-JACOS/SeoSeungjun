468cb3be444051e42e4221c1c671ac6edbc199e3
greensnapback0229 <smdmim@gmail.com>
2025-05-28 19:29:58 +0900

    solved: leetcode_841 - Keys and Rooms

---
+++ leetcode_841.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class leetcode_841 {
+    boolean[] visited;
+    int cnt = 0;
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        
+        int n = rooms.size();
+        visited = new boolean[n];
+
+        visited[0] = true;
+        visitRoom(rooms, 0);
+
+        return (cnt == n) ? true : false;
+    }
+
+    public void visitRoom(List<List<Integer>> rooms,int roomNum) {
+        cnt++;
+        List<Integer> keyList = rooms.get(roomNum);
+        
+        for(Integer key : keyList){
+            if(visited[key] == false){
+                visited[key] = true;
+                visitRoom(rooms, key);
+            }
+        }
+
+    }
+}
