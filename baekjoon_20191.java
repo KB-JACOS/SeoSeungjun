3ff6ccc5f2664c4fd5ebf5e2f7da0a625df36b2f
greensnapback0229 <smdmim@gmail.com>
2025-09-15 04:58:08 +0900

    solved: baekjoo_20191 - 줄임말

--- baekjoon_20191.java
+++ baekjoon_20191.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class baekjoon_20191 {
 
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
