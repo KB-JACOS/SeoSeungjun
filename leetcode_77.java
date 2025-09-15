a6b739e3a0ee6772eb60dd1d4a3a8f3c8e0dd77f
greensnapback0229 <smdmim@gmail.com>
2025-09-15 13:01:31 +0900

    solved: leetcode_77 - combinations

---
+++ leetcode_77.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class leetcode_77 {
+
+    List<List<Integer>> result =  new ArrayList<>();
+
+    public List<List<Integer>> combine(int n, int k) {
+        recur(n, k, 0, new ArrayList<Integer>(), 0);
+        return result;
+    }
+
+    public void recur(int n, int k, int depth, List<Integer> answer, int last) {
+        
+        if(depth == k) {
+            result.add(new ArrayList<>(answer));
+            return;
+        }
+
+        for(int i=last+1;i<=n;i++) {
+            answer.add(i);
+            recur(n, k, depth+1, answer, i);
+            answer.remove(answer.size() - 1 );
+        }
+    }
+}
