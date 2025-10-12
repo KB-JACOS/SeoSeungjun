c99d6d81507731fa218415399ae487fc76ccb88a
greensnapback0229 <smdmim@gmail.com>
2025-10-13 03:48:30 +0900

    solved: baekjoon_2169 - 로봇 조종하기

--- baekjoon_2169.java
+++ baekjoon_2169.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class baekjoon_2169 {
     
     static long[][] memo = new long[31][31];
     static final int LEFT = 0, RIGHT = 1, DOWN = 2;
