62fa2e20426158c478bbcb0195cf046fbd56e63e
greensnapback0229 <smdmim@gmail.com>
2025-10-14 17:03:37 +0900

    solved: baekjoon_12865 - 평범한 배낭

---
+++ baekjoon_12865.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class baekjoon_12865 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N, K; 
+        
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        int[][] stuff = new int[N+1][2];
+        int[][] dp = new int[N+1][K+1];
+        for(int i=1;i<=N;i++) {
+            st = new StringTokenizer(br.readLine());
+            stuff[i][0] = Integer.parseInt(st.nextToken());
+            stuff[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+
+
+        for(int i=1;i<=N;i++) {
+            int W = stuff[i][0];
+            int V = stuff[i][1];
+
+            for(int j=0;j<=K;j++){
+                if( j < W) {
+                    dp[i][j] = dp[i-1][j];
+                }
+                
+                else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i - 1][j - W] + V);
+                }
+            }
+        }
+
+        System.out.println(dp[N][K]);
+    }
+
+}
