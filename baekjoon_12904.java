40972a29dd49b10e45368314c0ffff7b291ebf1f
greensnapback0229 <smdmim@gmail.com>
2025-10-23 12:14:08 +0900

    solved: baekjoon_12904 - A와 B

--- baekjoon_12904.java
+++ baekjoon_12904.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class baekjoon_12904 {
     
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
