eaebc18ff02bfcebe3642c885bf66e3109e4e424
greensnapback0229 <smdmim@gmail.com>
2025-10-30 12:03:59 +0900

    solved: baekjoon_2490 - 모양 정돈

--- baekjoon_2490.java
+++ baekjoon_2490.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class baekjoon_2490 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
