d091815f40b9678b2334e80db97e3e6791556a46
greensnapback0229 <smdmim@gmail.com>
2025-11-14 03:15:42 +0900

    soled: swea_3752 - 가능한 시험 점수

--- swea_3752.java
+++ swea_3752.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-class Solution
+class swea_3752
 {
 	public static void main(String[] args) throws Exception {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
