c3a92fb294e84f2cf3c2ff635df69787841b3ffe
greensnapback0229 <smdmim@gmail.com>
2026-01-28 17:45:00 +0900

    solved: baekjoon_2448 - 별 찍기 - 11

--- baekjoon_2448.java
+++ baekjoon_2448.java
@@ -1,7 +1,7 @@
     import java.util.*;
     import java.io.*;
 
-    public class Main {
+    public class baekjoon_2448 {
         public static List<Integer> list = new ArrayList<>();
         public static boolean flag;
         public static boolean[] visited;
