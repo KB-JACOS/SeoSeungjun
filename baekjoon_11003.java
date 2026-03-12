cd45ee3c529b1a57c76d80b8a754f8a7caf42a8f
greensnapback0229 <smdmim@gmail.com>
2026-03-13 00:52:38 +0900

    solved: baekjoon_11003 - 최솟값 찾기

--- baekjoon_11003.java
+++ baekjoon_11003.java
@@ -1,3 +1,40 @@
+import java.io.*;
+import java.util.*;
+
 public class baekjoon_11003 {
-    
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        Deque<Integer> dq = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0;i<N;i++){
+
+            // 뒤에서 큰 값 제거
+            while(!dq.isEmpty() && arr[dq.peekLast()] > arr[i])
+                dq.pollLast();
+
+            dq.addLast(i);
+
+            // 범위 밖 제거
+            if(dq.peekFirst() <= i-L)
+                dq.pollFirst();
+
+            sb.append(arr[dq.peekFirst()]).append(" ");
+        }
+
+        System.out.println(sb);
+    }
 }
